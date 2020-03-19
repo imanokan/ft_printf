8bb402d0a333443b7b94e36e6a436a39f3ae8f78
imanokan <50735442+imanokan@users.noreply.github.com>
2020-03-19 22:23:49 +0100

    Create conversion_specifier.c

---
+++ conversion_specifier.c
@@ -0,0 +1,24 @@
+
+
+int     conversion_specifier(char spec)
+{
+    if (spec == 'c')
+        specifier_c;
+    else if (spec == 's')
+        specifier_s:
+    else if (spec == 'p')
+         specifier_p;
+    else if (spec == d' || spec == 'i') 
+          specifier_d;
+    else if (spec == 'o')
+          specifier_o;
+    else if (spec == 'u')
+          specifier_u;
+    else if (spec == 'x' ) 
+          specifier_x;
+     else if (spec == 'X')
+          specifier_X;
+      else if (spec == 'f')
+          specifier_f;
+      return (0);
+}
