42aeab60570ee0cf3547644a7d6754872bb5ba91
Audrey <audreymanoka@gmail.com>
2020-09-23 23:00:04 +0200

    %

--- format.c
+++ format.c
@@ -3,9 +3,11 @@
 
 int main ()
 {
-	char *s = "%%";
+//	char *s = ".";
+	int point = 0;
 	char *str = "%4%";
-	char *jo = "%%%";
+	int w = 0;
+	int width = 0;
 	char *a = "%05.4%";
 	int i = 0;
 	int j = 0;
@@ -17,8 +19,27 @@ int main ()
 	while (a[j] != '%')
 		j++;
 	spec = ft_strsub(a, i, j + 1);
-	printf(" spec : %s\n", spec);
+	i = 0;
+	while (spec[i] != '\0')
+	{
+		if (spec[i] == '.')
+			point = 1;
+		else if (spec[i] == ' ')
+			space = 1;
+		else if ((ft_isdigit(spec[i]) && point != 1))// all->spec[i - 1] != '.'))
+		{
+		 	w = 1;
+		 	width = ft_atoi(&spec[i]);
+		 	break;
+	 	}
+	 	i++;
+	}
+	//fill space
+//	printf("|%0.5%|\n");
+	printf(" width : %d\n", width);
 //	if spec = "%"
 //		print spec;
+	ft_putstr(spec);
+	free(spec);
 	return (0);
 }
--- specifiers_d.c
+++ specifiers_d.c
@@ -5,11 +5,8 @@
 int specifier_d(t_spec *all)
 {
 	int i;
-	signed int c;
-	char *s;
-	char *f;
 	i = 0;
-	//all->conv->d= va_arg(all->a_list, signed int);
+
 	all->conv->d = check_l_ll_h_hh(all->conv->d,all);
 	all->len_arg = size_nb(all->conv->d);
  	if (all->esp == 1 || all->plus == 1 || all->conv->d < 0)
@@ -33,30 +30,16 @@ int specifier_d(t_spec *all)
 
 int specifier_x(t_spec *all)
 {
-	//char *str;
 	int i;
 	i = 0;
 
-	//all->conv->x = va_arg(all->a_list,unsigned int);
 	all->conv->x = check_l_ll_h_hh_unsigned(all->conv->x,all);
 	all->conv->x_str = ft_itoa_base(all->conv->x,16);
-	//hash_x(all);
-	if (all->type == 'X') // voir l'autre ft_itoa_base avec c
+	if (all->type == 'X')
 			while (all->conv->x_str[i] != '\0')
 					ft_toupper(all->conv->x_str[i++]);
-	//printf(" str : %s\n",str );
-/*	if (all->hash == 1 && all->conv->x != 0)
-	{
-		if (all->type == 'X')
-			write(1,"0X",2);
-		else
-			write(1,"0x",2);
-		//check add la longeur +2
-	}*/
 	//fill number if needed et space en precision puis le ar
 	all->len_arg = ft_strlen(all->conv->x_str);
-//	if (all->hash == 1)
-//		all->len_arg += 2;
 	if (all->w == 1 && all->precision == 0)
 				width_s(all, all->conv->x_str);
  	else if (all->w == 0 && all->precision == 1)
@@ -70,7 +53,6 @@ int specifier_x(t_spec *all)
 
 int specifier_o(t_spec *all)
 {
-
 
 	all->conv->o = check_l_ll_h_hh_unsigned(all->conv->o, all);
  	all->conv->o_str= ft_itoa_base(all->conv->o,8);
@@ -94,7 +76,7 @@ int specifier_u(t_spec *all)
 		all->conv->u = UINT_MAX - all->conv->u;
 	all->len_arg = size_nb_unsigned(all->conv->u);
 	if (all->w == 1 && all->precision == 0)
-			width_signed(all, all->conv->u); // moyen de recup la vale
+			width_signed(all, all->conv->u);
 	else if (all->w == 0 && all->precision == 1)
 			precision_signed(all, all->conv->u);
 	else if (all->w == 1 && all->precision == 1)
